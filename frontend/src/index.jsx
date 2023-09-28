7ccdceab0bd1540fa71ca102b6e18b67205077be
MaxGre99 <maksonster99@gmail.com>
2023-09-29 02:45:14 +0600

    step #11.2

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
-// import Rollbar from 'rollbar';
-import {
-  Provider as RollbarProvider,
-  ErrorBoundary,
-} from '@rollbar/react';
+import Rollbar from 'rollbar';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import i18n from './i18n';
 import './styles/index.css';
 import App from './components/App.jsx';
@@ -14,24 +11,27 @@ import store from './slices/index';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const rollbarConfig = {
+const rollbarConfig = new Rollbar({
   accessToken: '754eab26a4db48c0a26588006a78b5ee',
-  environment: 'testenv',
-};
+  environment: 'production',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
 root.render(
-  <RollbarProvider config={rollbarConfig}>
-    <ErrorBoundary>
-      <React.StrictMode>
+  <React.StrictMode>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
         <I18nextProvider i18n={i18n} defaultNS="translation">
           <ReduxProvider store={store}>
             <App />
           </ReduxProvider>
         </I18nextProvider>
-      </React.StrictMode>
-    </ErrorBoundary>
-  </RollbarProvider>,
+      </ErrorBoundary>
+    </RollbarProvider>
+    ,
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
