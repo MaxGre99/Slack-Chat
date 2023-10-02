d5b8c46762c279a7b447f7cd3ac593eea7cd2422
MaxGre99 <maksonster99@gmail.com>
2023-10-02 17:26:44 +0600

    a little layout

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,8 +3,7 @@ const Messages = ({ chosenMessages }) => (
     {chosenMessages.map((message) => (
       <div className="text-break mb-2" key={message.id}>
         <b>{message.username}</b>
-        :
-        {' '}
+        {': '}
         {message.body}
       </div>
     ))}
