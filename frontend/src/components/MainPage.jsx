6f60b26b998847aacefec10d81b06d9ae3994f6f
MaxGre99 <maksonster99@gmail.com>
2023-10-02 17:15:27 +0600

    added disabling when message is sending

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -47,6 +47,7 @@ const MainPage = () => {
   const dispatch = useDispatch();
   const [chosenChannel, setChosenChannel] = useState({});
   const messageInput = useRef();
+  const [isSending, setSendingState] = useState(false);
   const userId = JSON.parse(localStorage.getItem('userId'));
   const getAuthHeader = () => {
     if (userId && userId.token) {
@@ -88,6 +89,7 @@ const MainPage = () => {
       username: userId.name,
     },
     onSubmit: (values) => {
+      setSendingState(true);
       let messageAcknowledged = false;
       socket.emit('newMessage', {
         body: filter.clean(values.body),
@@ -96,6 +98,7 @@ const MainPage = () => {
       }, (confirmation) => {
         if (confirmation.status === 'ok') {
           messageAcknowledged = true;
+          setSendingState(false);
         }
       });
       setTimeout(() => {
@@ -217,7 +220,16 @@ const MainPage = () => {
             <div className="mt-auto px-5 py-3">
               <Form noValidate className="py-1 border rounded-2" onSubmit={formik.handleSubmit}>
                 <InputGroup hasValidation={formik.values.body.length === 0 && true}>
-                  <Form.Control ref={messageInput} name="body" aria-label={t('forms.newMessage')} placeholder={t('forms.inputMessage')} className="border-0 p-0 ps-2" value={formik.values.body} onChange={formik.handleChange} />
+                  <Form.Control
+                    ref={messageInput}
+                    name="body"
+                    aria-label={t('forms.newMessage')}
+                    placeholder={t('forms.inputMessage')}
+                    className="border-0 p-0 ps-2"
+                    value={formik.values.body}
+                    onChange={formik.handleChange}
+                    disabled={isSending}
+                  />
                   <Button type="submit" variant="group-vertical" disabled={formik.values.body.length === 0 && true}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                       <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
