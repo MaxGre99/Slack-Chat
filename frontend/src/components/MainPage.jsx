31d6c105d079722e9a87a8cc1c3533bf7f47851f
MaxGre99 <maksonster99@gmail.com>
2023-10-02 22:32:20 +0600

    MessagesBox is now a whole new component

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -5,19 +5,19 @@ import {
   Row,
   Col,
   Button,
-  Form,
-  InputGroup,
+  // Form,
+  // InputGroup,
 } from 'react-bootstrap';
 import axios from 'axios';
 import io from 'socket.io-client';
-import { useFormik } from 'formik';
+// import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import filter from 'leo-profanity';
 import { actions as channelsActions } from '../slices/channelsSlice';
 import { actions as messagesActions } from '../slices/messagesSlice';
 import Channels from './Channels';
-import Messages from './Messages';
+import MessagesBox from './Messages';
 import getModal from '../modals/index.js';
 
 // Инициализация сокета
@@ -46,8 +46,6 @@ const MainPage = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
   const [chosenChannel, setChosenChannel] = useState({});
-  const messageInput = useRef();
-  const [isSending, setSendingState] = useState(false);
   const userId = JSON.parse(localStorage.getItem('userId'));
   const getAuthHeader = () => {
     if (userId && userId.token) {
@@ -79,55 +77,6 @@ const MainPage = () => {
       }
     };
     fetchData();
-  }, []);
-
-  // Настройки формы для сообщений
-  const formik = useFormik({
-    initialValues: {
-      body: '',
-      channelId: chosenChannel.id,
-      username: userId.name,
-    },
-    onSubmit: (values) => {
-      setSendingState(true);
-      socket.emit('newMessage', {
-        body: filter.clean(values.body),
-        channelId: values.channelId,
-        username: values.username,
-      }, (confirmation) => {
-        if (confirmation.status === 'ok') {
-          setSendingState(false);
-        }
-      });
-      setTimeout(() => {
-        if (isSending) {
-        // Если подтверждение не получено, обработайте это здесь
-          errorNotify(t('errors.connectionError'));
-        }
-      }, 5000);
-      formik.values.body = '';
-      messageInput.current.focus();
-    },
-  });
-
-  // useEffect на слежку за выбранным каналом + фокус-инпут (т.к. фокус тоже нужен при смене канала)
-  useEffect(() => {
-    if (chosenChannel && chosenChannel.id) {
-      formik.setFieldValue('channelId', chosenChannel.id);
-      messageInput.current.focus();
-    }
-  }, [chosenChannel]);
-
-  useEffect(() => {
-    document
-      .getElementById('input')
-      .addEventListener('keydown', (event) => {
-        if (event.code === 'Enter' || event.code === 'NumpadEnter') {
-          event.preventDefault();
-          // document.getElementById('submit').click();
-          // console.log('KURWA');
-        }
-      });
   }, []);
 
   // Cокеты
@@ -212,46 +161,15 @@ const MainPage = () => {
             allChannels={allChannels}
           />
         </Col>
-        <Col className="p-0 h-100">
-          <div className="d-flex flex-column h-100">
-            <div className="bg-light mb-4 p-3 shadow-sm small">
-              <p className="m-0">
-                <b>
-                  #
-                  {' '}
-                  {chosenChannel.name}
-                </b>
-              </p>
-              <span className="text-muted">
-                {t('count.messageCount', { count: chosenMessages.length })}
-              </span>
-            </div>
-            <Messages chosenMessages={chosenMessages} />
-            <div className="mt-auto px-5 py-3">
-              <Form noValidate className="py-1 border rounded-2" onSubmit={formik.handleSubmit}>
-                <InputGroup hasValidation={formik.values.body.length === 0 && true}>
-                  <Form.Control
-                    id="input"
-                    ref={messageInput}
-                    name="body"
-                    aria-label={t('forms.newMessage')}
-                    placeholder={t('forms.inputMessage')}
-                    className="border-0 p-0 ps-2"
-                    value={formik.values.body}
-                    onChange={formik.handleChange}
-                    disabled={isSending}
-                  />
-                  <Button type="submit" variant="group-vertical" disabled={formik.values.body.length === 0 && true} id="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-                      <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                    </svg>
-                    <span className="visually-hidden">{t('forms.send')}</span>
-                  </Button>
-                </InputGroup>
-              </Form>
-            </div>
-          </div>
-        </Col>
+        <MessagesBox
+          chosenMessages={chosenMessages}
+          chosenChannel={chosenChannel}
+          userId={userId}
+          socket={socket}
+          filter={filter}
+          errorNotify={errorNotify}
+          t={t}
+        />
       </Row>
       {modalIsOpen && (
         <ModalComponent
--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,13 +1,122 @@
-const Messages = ({ chosenMessages }) => (
-  <div id="messages-box" className="chat-messages overflow-auto px-5">
-    {chosenMessages.map((message) => (
-      <div className="text-break mb-2" key={message.id}>
-        <b>{message.username}</b>
-        {': '}
-        {message.body}
-      </div>
-    ))}
-  </div>
-);
+import React, { useEffect, useState, useRef } from 'react';
+import {
+  Col,
+  Form,
+  InputGroup,
+  Button,
+} from 'react-bootstrap';
+import { useFormik } from 'formik';
 
-export default Messages;
+const MessagesBox = ({
+  chosenMessages,
+  chosenChannel,
+  userId,
+  socket,
+  filter,
+  errorNotify,
+  t,
+}) => {
+  const messageInput = useRef();
+  const [isSending, setSendingState] = useState(false);
+
+  // Настройки формы для сообщений
+  const formik = useFormik({
+    initialValues: {
+      body: '',
+      channelId: chosenChannel.id,
+      username: userId.name,
+    },
+    onSubmit: (values) => {
+      setSendingState(true);
+      socket.emit('newMessage', {
+        body: filter.clean(values.body),
+        channelId: values.channelId,
+        username: values.username,
+      }, (confirmation) => {
+        if (confirmation.status === 'ok') {
+          setSendingState(false);
+        }
+      });
+      setTimeout(() => {
+        if (isSending) {
+        // Если подтверждение не получено, обработайте это здесь
+          errorNotify(t('errors.connectionError'));
+        }
+      }, 5000);
+      formik.values.body = '';
+    },
+  });
+
+  /* useEffect(() => {
+    document
+      .getElementById('input')
+      .addEventListener('keydown', (event) => {
+        if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+          event.preventDefault();
+          document.getElementById('submit').click();
+          console.log('KURWA');
+        }
+      });
+  }, []); */
+
+  // useEffect на слежку за выбранным каналом + фокус-инпут (т.к. фокус тоже нужен при смене канала)
+  useEffect(() => {
+    if (chosenChannel && chosenChannel.id) {
+      formik.setFieldValue('channelId', chosenChannel.id);
+      messageInput.current.focus();
+    }
+  }, [chosenChannel, chosenMessages]);
+
+  return (
+    <Col className="p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">
+            <b>
+              #
+              {' '}
+              {chosenChannel.name}
+            </b>
+          </p>
+          <span className="text-muted">
+            {t('count.messageCount', { count: chosenMessages.length })}
+          </span>
+        </div>
+        <div id="messages-box" className="chat-messages overflow-auto px-5">
+          {chosenMessages.map((message) => (
+            <div className="text-break mb-2" key={message.id}>
+              <b>{message.username}</b>
+              {': '}
+              {message.body}
+            </div>
+          ))}
+        </div>
+        <div className="mt-auto px-5 py-3">
+          <Form noValidate className="py-1 border rounded-2" onSubmit={formik.handleSubmit}>
+            <InputGroup hasValidation={formik.values.body.length === 0 && true}>
+              <Form.Control
+                // id="input"
+                ref={messageInput}
+                name="body"
+                aria-label={t('forms.newMessage')}
+                placeholder={t('forms.inputMessage')}
+                className="border-0 p-0 ps-2"
+                value={formik.values.body}
+                onChange={formik.handleChange}
+                disabled={isSending}
+              />
+              <Button type="submit" variant="group-vertical" disabled={formik.values.body.length === 0 && true}/* id="submit" */>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+                  <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                </svg>
+                <span className="visually-hidden">{t('forms.send')}</span>
+              </Button>
+            </InputGroup>
+          </Form>
+        </div>
+      </div>
+    </Col>
+  );
+};
+
+export default MessagesBox;
